125eeb89366fb1b95a226021680123f2424cde29
hhk9292 <hhk9292@gmail.com>
2020-09-27 16:38:09 +0900

    edit sql | Sep 27, 2020

--- backend/routes/users/controller.js
+++ backend/routes/users/controller.js
@@ -2,7 +2,7 @@ const db = require('../../database')
 const conn = db.init()
 
 exports.userlist = (req, res, next) => {
-    const sql = 'select * from users';
+    const sql = 'select id from users';
     conn.query(sql, (err, rows, fields) => {
         if (err) {
             console.log('query is not executed' + err)
